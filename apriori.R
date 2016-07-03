cf6c28ef380a65a78b9e138116787f7d1bf05000
Grégoire RIVIERE <griviere42@gmail.com>
2016-07-03 21:21:18 +0200

    debug ok, still have to check results

--- apriori.R
+++ apriori.R
@@ -1,48 +1,76 @@
 Apriori<-function(data_set, threshold=50){
   
-  sy = dim(data_set)[1];
-  sx = dim(data_set)[2];
+  data_fac <- data.frame(lapply(data_set, factor));
+  sy = dim(data_fac)[1];
+  sx = dim(data_fac)[2];
   pattern = vector("character");
-  candidates = vector();
+  candidates_old = list();#old candidates
+  candidates_new = list();#new candidates
+  candidates_1 = list();#candidates with size 1 to generate patterns
   tempo_fac = factor();
-  tempo_num = vector();
   
   for(size_pat in 1:sx){
     
     if(size_pat ==1){
       
       for(col in 1:sx){
-        
-        if(class(data_set[,col])=="factor"){
           
-          tempo_fac <- levels(data_set[,col]);
+          tempo_fac <- as.factor(levels(data_fac[,col]));
           
           for(possibilities in 1:length(tempo_fac)){
             
-            if(sum(data_set[,col]==tempo_fac[possibilities], na.rm=TRUE) >= threshold)
-              pattern <- c(pattern, paste(names(data_set)[col], 
+            if(sum(data_fac[,col]==tempo_fac[possibilities], na.rm=TRUE) >= threshold){
+              pattern <- c(pattern, paste(names(data_fac)[col], 
                                           " = ", as.character(tempo_fac[possibilities])));
-            
+              candidates_old <- c(candidates_old, list(list(col, tempo_fac[possibilities])));
+              candidates_1 = candidates_old;
+            }
           }
-        }
-        else if(class(data_set[,col])=="integer"){
-          
-          tempo_num <- unique(data_set[,col]);
-          
-          for(possibilities in 1:length(tempo_num)){
-            
-            if(sum(data_set[,col]==tempo_num[possibilities], na.rm=TRUE) >= threshold)
-              pattern <- c(pattern, paste(names(data_set)[col], 
-                                          " = ", as.character(tempo_num[possibilities])));
-            
-          }
-          
-        }
       }
+      
+    }
+    
+    else{
+        
+      for(cand in 1:length(candidates_old)){
+        
+        for(cand1 in 1:length(candidates_1)){
+          
+          if(all(candidates_old[[cand]][[1]] != candidates_1[[cand1]][[1]])){
+            #checking if we are not dealing with the same col
+            tempo_fac <- data_fac;
+            for(i in 1:length(candidates_old[[cand]][[1]])){
+              tempo_fac <- tempo_fac[tempo_fac[,candidates_old[[cand]][[1]][i]]==candidates_old[[cand]][[2]][i],];
+            }
+            tempo_fac <- tempo_fac[tempo_fac[,candidates_1[[cand1]][[1]]]==candidates_1[[cand1]][[2]],];
+            if(nrow(tempo_fac)>= threshold){#the pattern has to be added
+              candidates_new <- c(candidates_new, list(c(candidates_old[[cand]][[1]], candidates_1[[cand1]][[1]]), 
+                                                       c(candidates_old[[cand]][[2]], candidates_1[[cand1]][[2]])));
+            }
+            
+          }
+          
+        }
+        
+      }
+      
+      candidates_old <- candidates_new;
+      candidates_new <- list();
+      
+      for(pat in 1:length(candidates_old)){
+        for(col in candidates_old[[pat]][1]){
+         pattern <- c(pattern, paste(names(data_fac[,col])[col], 
+                            " = ", as.character(candidates_old[[pat]][[2]][candidates_old[[pat]][[1]]==col]), " "));
+      
+        }
+        if(pat !=1){
+          pattern <- c(pattern, " / ");
+        }
       
     }
     
   }
   pattern
   
+  }
 }
